36a026271265b3c287474f4a7db2512e0d15c54f
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-07-19 15:39:19 +0200

    create variables to hold day, month , year and century values.

--- akan.js
+++ akan.js
@@ -1,3 +1,7 @@
 function get(){
+   var dd = parseFloat(document.getElementById('findday').value);
+   var mm = parseFloat(document.getElementById('findmonth').value);
+   var yy = parseFloat(document.getElementById('year').value);
+   var cc = (yy-1) / 100 + 1;
    
 }
