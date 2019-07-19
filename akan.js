192b637b44971da28fd5b59d2199f02d375a7e8b
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-07-19 14:42:23 +0200

    remove duplicate folder in css folder.

---
+++ akan.js
@@ -0,0 +1,31 @@
+function get(){
+var dd = parseFloat(document.getElementById('findday').value);
+var mm = parseFloat(document.getElementById('findmonth').value);
+var yy = parseFloat(document.getElementById('year').value);
+var cc = (yy-1)/100 + 1;
+var maleNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var femaleNames = ['Akosua','Adwoa','Abena','Akua','Yaa','Afua','Ama'];
+var dayOfWeek =  Math.round(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7)
+var gender 
+var checkedValue = document.getElementById("find").value;
+
+//alert(checkedValue);
+if (checkedValue === "male") {
+  //maleNames.push(dayOfWeek.toFixed())
+ console.log(dayOfWeek)
+// alert(maleNames[Math.round(dayOfWeek)])
+//  document.getElementsByClassName('chckmale').innerHTML= maleNames[Math.round(dayOfWeek)];
+ if (dayOfWeek === 7){
+   document.getElementById('chckmale').innerHTML= maleNames[0];
+   console.log(maleNames)
+ }
+  }
+  else {
+  console.log(Math.round(dayOfWeek))
+  document.getElementById('chckmale').innerHTML=femaleNames[dayOfWeek];
+  if (dayOfWeek === 7){
+   document.getElementById('chckmale').innerHTML=  femaleNames[0];
+   console.log(femaleNames)
+ }
+}
+}
