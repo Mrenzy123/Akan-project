4704d0f82919eb90558a2fd745d3a4bedf260962
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-07-19 15:58:33 +0200

    create an else statement to define female information if male isn't checked.

--- akan.js
+++ akan.js
@@ -4,10 +4,21 @@ function get(){
    var yy = parseFloat(document.getElementById('year').value);
    var cc = (yy-1) / 100 + 1;
    var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-   var femalenames = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
-   var dayOfWeek = Math.round( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+   var femaleNames = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
+   var dayOfWeek = Math.round( ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
    var checkedValue = document.getElementById('find').value;
    if (checkedValue === "male"){
-      
+      console.log(dayOfWeek);
+      if (dayOfWeek === 7){
+         document.getElementById('chckmale').innerHTML= maleNames[0];
+         console.log(maleNames);
+      }
+   } else {
+      console.log(Math.round(dayOfWeek));
+      document.getElementById('chckmale').innerHTML= femaleNames[dayOfWeek];
+      if (dayOfWeek === 7){
+         document.getElementById('chckmale').innerHTML= femaleNames[0];
+         console.log(femaleNames);
+      }
    }
 }
