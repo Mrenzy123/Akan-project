3e1f6971631793ee10ed9bed9d5e039192a06894
Mrenzy123 <deborahmurenzi94@gmail.com>
2019-07-19 15:47:36 +0200

    create variables for dayofweek and checking gender value.

--- akan.js
+++ akan.js
@@ -5,4 +5,7 @@ function get(){
    var cc = (yy-1) / 100 + 1;
    var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
    var femalenames = ['Akosua', 'Adwoa', 'Abena', 'Akua', 'Yaa', 'Afua', 'Ama'];
+   var dayOfWeek = Math.round( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+   var checkedValue = document.getElementById('find').value;
+   
 }
